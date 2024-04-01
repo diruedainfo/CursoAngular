f4ba5403d70ee82e98a963f05433afc0fb65937c
drueda <diego.rueda@igmweb.com>
2024-04-01 17:34:06 +0200

    lista de agencias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CursoAngular';
+  public title = 'Astro Booking';
+  public subtitle = 'Welcome on board';
+  public author = 'Diego Rueda';
+  public authorUrl = 'http://www.google.es';
+  public agencies = [
+    {
+      id: 'space-y',
+      name: 'Space Y',
+      range: 'Interplanetary',
+      status: 'Active',
+    },
+    {
+      id: 'green-origin',
+      name: 'Green Origin',
+      range: 'Orbital',
+      status: 'Active',
+    },
+    {
+      id: 'virgin-way',
+      name: 'Virgin Way',
+      range: 'Orbital',
+      status: 'Pending',
+    },
+  ]
+  public reloading = false;
+  public getAgenciesLength() {
+    return this.agencies.length;
+  }
+
+  public reload(list:string) {
+    this.reloading = true;
+    console.log('Reloading...' + list);
+  }
 }
