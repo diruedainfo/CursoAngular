43fffee8363be8af9ae25775c625ade35236ccc7
drueda <diego.rueda@igmweb.com>
2024-04-03 10:33:16 +0200

    feature: contact form generated

---
+++ src/app/contact/contact.form.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact.form.html',
+  styleUrls: ['./contact.form.scss']
+})
+export class ContactForm implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
