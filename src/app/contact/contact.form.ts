e693a6e1ed914dd583bee13598cd83005ccc25b0
drueda <diego.rueda@igmweb.com>
2024-04-03 13:28:26 +0200

    feature: form validation

--- src/app/contact/contact.form.ts
+++ src/app/contact/contact.form.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 interface Contact {
@@ -18,11 +18,50 @@ export class ContactForm implements OnInit {
   public form : FormGroup;
   constructor(formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      name:['', Validators.required],
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(50),
+      ]),
     });
   }
 
   ngOnInit(): void {}
+
+  public hasError(controlName: string): boolean {
+    const control = this.getControl(controlName);
+    if (!control) return false;
+    return control.invalid;
+  }
+
+  public mustShowMessage(controlName: string): boolean {
+    const control = this.getControl(controlName);
+    if (!control) return false;
+    return control.invalid && control.touched;
+  }
+
+  public getControl(controlName: string): AbstractControl | null {
+    return this.form.get(controlName);
+  }
+
+  public getErrorMessage(controlName: string): string {
+    const control = this.getControl(controlName);
+    if (!control) return 'no existe';
+    if(!control.errors) return '';
+    const errors = control.errors;
+    let errorMessage = '';
+    errorMessage += errors['required'] ? '🔥 Field is required' : '';
+    errorMessage += errors['email'] ? '🔥 Should be an email address' : '';
+    errorMessage += errors['minlength']
+      ? `🔥 More than ${errors['minlength'].requiredLength} chars`
+      : '';
+    errorMessage += errors['maxlength']
+      ? `🔥 Less than ${errors['maxlength'].requiredLength} chars`
+      : '';
+    return errorMessage;
+  }
 
   public onSave(){
     const contact = this.form.value;
