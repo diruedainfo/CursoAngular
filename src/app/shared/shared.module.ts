473927544e1bb59401de1171c66d0fefa95582cc
drueda <diego.rueda@igmweb.com>
2024-04-03 08:45:46 +0200

    feature: router link

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReloadingComponent } from './reloading/reloading.component';
 import { AgenciesLista } from './components/agencies/agencies.lista';
 import { TripsLista } from './components/trips/trips.lista';
+import {Router, RouterModule} from "@angular/router";
 
 
 
@@ -13,9 +14,11 @@ import { TripsLista } from './components/trips/trips.lista';
     TripsLista
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
   exports: [
+    RouterModule,
     ReloadingComponent,
     AgenciesLista,
     TripsLista
