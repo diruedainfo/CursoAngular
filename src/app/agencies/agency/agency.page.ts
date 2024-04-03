4b55ee1d6446798d3ca6d2a9c2d68f1339d57e80
drueda <diego.rueda@igmweb.com>
2024-04-03 09:11:06 +0200

    feature: get id from activated route

--- src/app/agencies/agency/agency.page.ts
+++ src/app/agencies/agency/agency.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-agency',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgencyPage implements OnInit {
 
-  constructor() { }
+  public agencyId: string;
+
+  constructor(route:ActivatedRoute) {
+    this.agencyId = route.snapshot.paramMap.get('id') || '';
+  }
 
   ngOnInit(): void {
   }
