b379a2d38a8d5652334e6950c807064cf175b07f
drueda <diego.rueda@igmweb.com>
2024-04-15 10:02:57 +0200

    Features: PWA update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwUpdate, VersionEvent} from "@angular/service-worker";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  public newVersion = '';
 
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+      if (event.type === 'VERSION_READY') {
+        this.newVersion = event.latestVersion.appData
+          ? JSON.stringify(event.latestVersion.appData)
+          : event.latestVersion.hash;
+      }
+    });
+    swUpdate.checkForUpdate();
+    //interval(1000 * 60 * 60).subscribe(() => swUpdate.checkForUpdate());
+  }
+  public onReload() {
+    window.location.reload();
+  }
 }
